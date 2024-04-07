89103605140c860bd68f513aacdaa50904856097
farhanpro <shaihfarhan549@gmai.com>
2024-04-07 17:33:12 +0530

    Episode 1

--- App.js
+++ App.js
@@ -1,25 +1,8 @@
 const parent = React.createElement(
     "div",
-    {id : "parent"},
-    React.createElement(
-    "div",
-    {id:"child"},
-
-    [
-    React.createElement(
-    "h1",
-    {},
-    "This is an H1 tag"),   
-
-]
-    ));
-
-
-// const heading = React.createElement(
-//     "h1",
-//     {id:"heading",xyz:"abc"},
-//     "Hello world from React!"
-// );
+    {id:"parent"},
+    React.createElement("div",{id:"child"},React.createElement("h1",{},"I'm an H1 tag"))
+)
 
 //root.render(heading); //object
 const root = ReactDOM.createRoot(document.getElementById("root"));
